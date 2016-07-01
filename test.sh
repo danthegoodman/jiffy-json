e476c98512bb5bd2683a7dd9683aa9a3404f8b1e
Danny Kirchmeier <danny@kirchmeier.us>
2016-06-30 22:29:33 -0500

    Remove '[]' push syntax

--- test.sh
+++ test.sh
@@ -46,6 +46,7 @@ echo "--- Objects ---"
 doPass "colon in key"             '{"a:b":"x"}'          'a\:b':x
 doPass "dot in key"               '{"a.b":100}'          'a\.b':100
 doPass "array notation in key"    '{"[foo]":"x"}'        [foo]:x
+doPass "array notation in key"    '{"[]":"x"}'           []:x
 doPass "bracket without escaping" '{"[0]":"x"}'          '\[0]':x
 doPass "(s) in key"               '{"x(s)":100}'         'x\(s)':100
 doPass "empty key"                '{"":"x"}'             :x
@@ -54,21 +55,18 @@ doPass "empty key in path"        '{"a":{"":{"b":"x"}}}' a..b:x
 echo
 
 echo "--- Array ---"
-doPass "push syntax"           '[1,2,3]'          []:1  []:2  []:3
 doPass "indexed"               '[1,2,3]'          [0]:1 [1]:2 [2]:3
 doPass "indexed unordered"     '[3,1,2]'          [1]:1 [2]:2 [0]:3
 doPass "indexed with gaps"     '[null,null,"x"]'  [2]:x
-doPass "mix push and indexed"  '[null,2,1,3]'     [1]:2 []:1  []:3
 echo
 
 echo "--- Arrays in Arrays --- "
-doPass "push syntax"   '[[1,2],[3,4],[5,6]]'  [0][]:1  [0][]:2  [1][]:3  [1][]:4  [2][]:5  [2][]:6
 doPass "indexed"       '[[1,2],[3,4],[5,6]]'  [0][1]:2 [0][0]:1 [2][0]:5 [2][1]:6 [1][0]:3 [1][1]:4
 echo
 
 echo "--- Arrays in Objects ---"
-doPass "push syntax"     '{"a":[1,2,3]}'      a[]:1   a[]:2   a[]:3
-doPass "indexed"         '{"a":[3,2,1]}'      a[2]:1  a[1]:2  a[0]:3
+doPass "ordered"           '{"a":[3,2,1]}'      a[0]:3  a[1]:2  a[2]:1
+doPass "unordered"         '{"a":[3,2,1]}'      a[2]:1  a[1]:2  a[0]:3
 echo
 
 echo "--- Objects in Arrays ---"
@@ -86,15 +84,14 @@ echo
 echo "--- Combining multiple concepts ---"
 doPass "Sample 1" \
        '{"a":{"b":[{"c":1,"d":4.3,"e":false},true,"x"],"x":null},"y":["100"],"z":"z"}' \
-       a.b[0].c:1  a.b[0].d:4.3  a.b[0].e:false  a.b[1]:true  a.b[2]:x  a.x:null  'y[](s):100'  z:z
+       a.b[0].c:1  a.b[0].d:4.3  a.b[0].e:false  a.b[1]:true  a.b[2]:x  a.x:null  'y[0](s):100'  z:z
 echo
 
 #      expected error message                         invalid input
 echo "--- Errors ---"
 doFail "unable to parse argument"                     a
-doFail "cannot treat array as object"                 a[]:1  a.x:2
-doFail "cannot treat object as array"                 a.x:2  a[]:1
-doFail "push syntax can only be used at end of path"  a[].b:1
+doFail "cannot treat array as object"                 a[0]:1  a.x:2
+doFail "cannot treat object as array"                 a.x:2  a[0]:1
 doFail "array index is already set"                   a[0]:1  a[0]:2
 doFail "object property is already set"               a.b:1  a.b:2
 
